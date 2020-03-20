8da901c3c2fd04082c25b0e44d7bb6b9c8fcfef3
Jason Ma <jason.ma@airbnb.com>
2020-03-19 20:57:13 -0700

    Adding react-dom

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import { Container, Header } from 'semantic-ui-react';
-import BdcMenu from './Menu';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import Landing from "./Landing";
+import NotFound from "./NotFound";
 
 class App extends Component {
   constructor(props) {
@@ -35,40 +36,12 @@ class App extends Component {
 
   render() {
     return (
-        <div>
-          <BdcMenu></BdcMenu>
-          <Container text>
-            <Header as='h2'>Blue Dress Capital</Header>
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-              ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-              magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-              ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-              quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-              arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-              Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-              dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-              Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-              Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-              viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-              Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-              ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-              magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-              ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-              quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-              arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-              Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-              dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-              Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-              Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-              viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-              Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-            </p>
-          </Container>
-        </div>
+        <BrowserRouter>
+          <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
     );
   }
 }
--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react';
+import BdcMenu from "./Menu";
+import {Link} from 'react-router-dom';
 
-export default class Menu extends Component {
+export default class Landing extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hello world!!</h1>
+                <Link to={"/contact"}> click here</Link> to contact us.
+            </div>
+        );
+    }
 
 }

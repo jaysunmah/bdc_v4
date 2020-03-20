dee2f3f9117f6251a4703827b44cc75b47237c7c
Jason Ma <jason.ma@airbnb.com>
2020-03-20 15:42:47 -0700

    wip, there seems to be a bug in isPrime

--- frontend/actions/menu.js
+++ frontend/actions/menu.js
@@ -3,3 +3,16 @@ export const selectItem = (item) => {
     dispatch({ type: "SELECT_ITEM", item });
   }
 };
+
+export const checkIsPrime = num => {
+  return (dispatch) => {
+    let headers = {"Content-Type": "application/json"};
+    let body = JSON.stringify({ num });
+    return fetch("/api/tdameritrade/helloworld", { headers, body, method: "POST"})
+      .then(res => {
+        return res.json().then(data => {
+          dispatch({type: "IS_PRIME_RES", res: data.res})
+        })
+      })
+  }
+}
--- frontend/reducers/menu.js
+++ frontend/reducers/menu.js
@@ -1,11 +1,14 @@
 const initialState = {
-  active: "home"
+  active: "home",
+  isPrime: false,
 }
 
 export default function menu(state=initialState, action) {
   switch (action.type) {
     case 'SELECT_ITEM':
       return {...state, active: action.item};
+    case 'IS_PRIME_RES':
+      return {...state, isPrime: action.res };
     default:
       return state;
   }

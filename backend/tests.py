2d1a4d50700c892a179287bf1146726ba881bd90
rsdutta <rsdutta2@illinois.edu>
2020-03-20 19:47:58 -0400

    fixing indentation

--- backend/tests.py
+++ backend/tests.py
@@ -7,3 +7,4 @@ class AnimalTestCase(TestCase):
 	def test_isPrime(self):
 		self.assertEqual(helloworld.isPrime(3), True)
 		self.assertEqual(helloworld.isPrime(4), False)
+		self.assertEqual(helloworld.isPrime(9), False)

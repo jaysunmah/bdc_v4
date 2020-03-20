49ac6c7d29108a012239cdd23e2b5d970aa46cfa
rsdutta <rsdutta2@illinois.edu>
2020-03-20 19:43:20 -0400

    seeing if tests work

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+from backend.modules.tdameritrade import helloworld
+
+class AnimalTestCase(TestCase):
+	def test_isPrime(self):
+		self.assertEqual(helloworld.isPrime(3), True)
+		self.assertEqual(helloworld.isPrime(4), False)

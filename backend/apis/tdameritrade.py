4acf01ddcbb666665d3a0c609551fd5b136b4f4d
rsdutta <rsdutta2@illinois.edu>
2020-03-20 22:20:12 -0400

    this will fail

--- backend/apis/tdameritrade.py
+++ backend/apis/tdameritrade.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from backend.modules.tdameritrade import helloworld
 
 class HelloWorldAPI(generics.GenericAPIView):
+    print(1/0)
     def get(self, request):
         print(helloworld.helperFn())
         return Response({

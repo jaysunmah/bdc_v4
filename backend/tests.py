b9364bd97d3161735de970b4dbdabb86bfc0ffc2
rsdutta <rsdutta2@illinois.edu>
2020-03-20 21:39:54 -0400

    jason uses spaces apparently

--- backend/tests.py
+++ backend/tests.py
@@ -4,8 +4,8 @@ from backend.modules.tdameritrade import helloworld
 # Create your tests here.
 
 class AnimalTestCase(TestCase):
-	def test_isPrime(self):
-		self.assertEqual(helloworld.isPrime(3), True)
-		self.assertEqual(helloworld.isPrime(4), False)
-		self.assertEqual(helloworld.isPrime(9), False)
-		self.assertEqual(helloworld.isPrime('test'), False)
+    def test_isPrime(self):
+        self.assertEqual(helloworld.isPrime(3), True)
+        self.assertEqual(helloworld.isPrime(4), False)
+        self.assertEqual(helloworld.isPrime(9), False)
+        self.assertEqual(helloworld.isPrime('test'), False)

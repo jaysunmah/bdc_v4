71586815e351d0ca684fc974b1a3ab66bb8c44c2
rsdutta <rsdutta2@illinois.edu>
2020-03-20 20:00:48 -0400

    does this fix spacing?

--- backend/tests.py
+++ backend/tests.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
+from backend.modules.tdameritrade import helloworld
 
 # Create your tests here.
-from backend.modules.tdameritrade import helloworld
 
 class AnimalTestCase(TestCase):
 	def test_isPrime(self):
 		self.assertEqual(helloworld.isPrime(3), True)
 		self.assertEqual(helloworld.isPrime(4), False)
 		self.assertEqual(helloworld.isPrime(9), False)
-		self.assertEqual(helloworld.isPrime('test', False)
+		self.assertEqual(helloworld.isPrime('test'), False)

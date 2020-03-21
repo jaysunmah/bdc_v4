f2f0dc126671a64dc113232fa6bcda0124182a38
rsdutta <rsdutta2@illinois.edu>
2020-03-20 22:10:12 -0400

    e2e test instead of unit

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 # Create your tests here.
+
+class e2eTest(StaticLiveServerTestCase):
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def tearDownClass(cls):
+        super().tearDownClass()

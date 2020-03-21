d4cfadfa727cdf2bef4c167390fdca1096500942
rsdutta <rsdutta2@illinois.edu>
2020-03-20 22:30:16 -0400

    adding integration test to master

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+  
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 # Create your tests here.
+
+class e2eTest(StaticLiveServerTestCase):
+    def setUpClass(self):
+        super().setUpClass()
+
+    def tearDownClass(self):
+        super().tearDownClass()

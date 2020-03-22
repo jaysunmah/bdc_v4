46693c623f7f24a5f9fcb0e01d2397e08b3e7caf
Jason Ma <jason.ma@airbnb.com>
2020-03-21 20:41:33 -0700

    Add backend api to create, update, and fetch td account

--- backend/tdameritrade/models.py
+++ backend/tdameritrade/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class TDUser(models.Model):
+class TDAccount(models.Model):
     bdc_user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     refresh_token = models.CharField(max_length=1000)
     access_token = models.CharField(max_length=1000)
--- backend/tdameritrade/serializers.py
+++ backend/tdameritrade/serializers.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers
-
+from .models import TDAccount
 
 class HelloWorldSerializer(serializers.Serializer):
     num = serializers.IntegerField()
+
+class TDAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TDAccount
+        fields = ('bdc_user', 'refresh_token', 'access_token', 'account_id', 'client_id')
+
+class TDAccountAPISerializer(serializers.Serializer):
+    refresh_token = serializers.CharField()
+    access_token = serializers.CharField()
+    account_id = serializers.CharField()
+    client_id = serializers.CharField()

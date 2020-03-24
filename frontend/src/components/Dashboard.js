46269f592e0ea2d71704e856b385d2e02389ed06
rsdutta <rsdutta2@illinois.edu>
2020-03-23 22:37:56 -0400

    added basic grid to dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import BdcContainer from "./BdcContainer";
 import { dashboard } from "../../actions";
 import { connect } from "react-redux";
+import { Grid, Segment } from 'semantic-ui-react'
 
 class Dashboard extends Component {
 
@@ -13,7 +14,24 @@ class Dashboard extends Component {
     return (
       <BdcContainer>
         <h1>Dashboard</h1>
+          <Grid columns={3} >
+            <Grid.Row stretched>
+              <Grid.Column>
+                <Segment>1</Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment>1</Segment>
+                <Segment>2</Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment>1</Segment>
+                <Segment>2</Segment>
+                <Segment>3</Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
       </BdcContainer>
+      
     );
   }
 }

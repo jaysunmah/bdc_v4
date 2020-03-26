ce8df3fe138b60ffba7d0c7efa4abfc4856c923d
Jason Ma <jason.ma@airbnb.com>
2020-03-25 22:59:31 -0700

    Change primary palette to blue

--- frontend/src/components/Theme.js
+++ frontend/src/components/Theme.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { createMuiTheme } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/red';
+import { blue } from '@material-ui/core/colors';
 
 export const muiTheme = createMuiTheme({
   palette: {
-    primary: red
+    primary: blue
   }
 });
